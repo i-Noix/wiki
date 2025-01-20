89f561e5bcc998e303390c6d314a4ba91ee5cd99
Roman Nazarov <rnazarov05@gmail.com>
2025-01-20 13:09:03 +0200

    Виконує вимоги щодо відвідування сторінки запису

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, title):
+    page_exist = util.get_entry(title)
+    if page_exist:
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            "entrie": markdown2.markdown(page_exist)
+        })
+    else:
+        return render(request, "encyclopedia/title.html", {
+            "title": title + " page not found"
+        })
